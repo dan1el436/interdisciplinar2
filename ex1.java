ade0a03822e64ea0d8231aa43db0fff63a34f74a
dan1el436 <105385518+dan1el436@users.noreply.github.com>
2022-08-24 22:26:05 -0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,14 @@
+public class ex1 {
+    public static void main(String[] args) {
+        int soma = num(50,100);
+        System.out.println(soma);
+
+    }
+
+    public static int num(int n1, int n2){
+        return n1+n2;
+
+    }
+
+   
+}
